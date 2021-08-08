cc8fa3211dafe3d03951625a3664066a78a82194
Mateus Vitor <mateusriograndense@gmail.com>
2021-08-08 14:45:11 -0300

    :poop: Função de popular o banco de dados incompleta.

--- redothis/extensions/cli.py
+++ redothis/extensions/cli.py
@@ -2,11 +2,22 @@ import click
 from ..misc import populate_database as pop
 from ..extensions.database import database as db
 
+
 def init(app):
 
     @app.cli.command()
-    def initdb():
-        """Populate your test database to development enviroment."""
+    def init_database():
+        """
+        Função responsável pela criação das tabelas do banco de dados
+        e todas as suas relações definidas na camada de modelos.
+        """
         db.create_all()
-        pop.init()
         click.echo("Initialized the database")
+
+    @app.cli.command()
+    def populate_database():
+        """
+        Função responsável pela inserção de dados de teste no banco de
+        dados anteriormente criado. Esta função requer a init_database.
+        """
+        pass
