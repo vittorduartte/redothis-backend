a75928c5e97eced52bdce83ea5d6101f44878365
Mateus Vitor <mateusriograndense@gmail.com>
2021-08-08 14:42:52 -0300

    :sparkles: Adição de autenticação nos endpoints e correção dos imports

--- redothis/resources/categories.py
+++ redothis/resources/categories.py
@@ -1,11 +1,17 @@
-from flask import request, jsonify, Blueprint
-from ..crud.category import register_category, get_all_categories, get_category_by_id
-import os 
+from flask import request
+from flask import jsonify
+from flask import Blueprint
+from ..controllers.category import get_all_categories
+from ..controllers.category import get_category_by_id
+from ..controllers.category import register_category
+from flask_jwt import jwt_required
+
 
 def init(app):
     bp = Blueprint('categories', __name__)
 
     @bp.route('/category', methods=['GET', 'POST'])
+    @jwt_required()
     def register():
         if request.method == 'POST':
             return register_category()
@@ -13,9 +19,8 @@ def init(app):
             return get_category_by_id()
 
     @bp.route('/categories', methods=['GET'])
+    @jwt_required()
     def get_categories():
         return get_all_categories()
-    
-    app.register_blueprint(bp, url_prefix="/api/v1")
 
-    
+    app.register_blueprint(bp, url_prefix="/api/v1")
