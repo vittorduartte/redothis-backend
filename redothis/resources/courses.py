c9de31805d095e2b7384fe47ee676b90ac6d63ef
Mateus Vitor <mateusriograndense@gmail.com>
2021-08-08 14:43:53 -0300

    :sparkles: Adição de autenticação nos endpoints e correção dos imports.

--- redothis/resources/courses.py
+++ redothis/resources/courses.py
@@ -1,11 +1,18 @@
-from flask import jsonify, request, Blueprint
-from ..crud.course import register_course, get_all_courses, get_course_by_id
+from flask import jsonify
+from flask import request
+from flask import Blueprint
+from ..controllers.course import get_all_courses
+from ..controllers.course import get_course_by_id
+from ..controllers.course import register_course
+from flask_jwt import jwt_required
+
 
 def init(app):
 
     bp = Blueprint('courses', __name__)
 
     @bp.route('/course', methods=['GET', 'POST'])
+    @jwt_required()
     def register():
         if request.method == 'POST':
             return register_course()
@@ -15,5 +22,5 @@ def init(app):
     @bp.route('/courses', methods=['GET'])
     def get_courses():
         return get_all_courses()
-    
+
     app.register_blueprint(bp, url_prefix="/api/v1")
