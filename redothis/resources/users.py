cda38fc8fee011bb86f0c1ae5b725d35b04f5c48
Mateus Vitor <mateusriograndense@gmail.com>
2021-08-08 20:16:35 -0300

    :bug: Correção do nome da função para selecionar usuário por email.

--- redothis/resources/users.py
+++ redothis/resources/users.py
@@ -2,7 +2,7 @@ from flask import jsonify
 from flask import request
 from flask import Blueprint
 from ..controllers.users import get_students_by_course
-from ..controllers.users import get_user_by_id
+from ..controllers.users import get_user_by_email
 from ..controllers.users import register_user
 from ..controllers.users import auth_user
 from ..controllers.projects import get_projects_by_user
@@ -22,7 +22,7 @@ def init(app):
         if request.method == 'POST':
             return register_user()
         else:
-            return get_user_by_id()
+            return get_user_by_email()
 
     @bp.route('/course/<int:id_course>/users', methods=['GET'])
     @jwt_required()
