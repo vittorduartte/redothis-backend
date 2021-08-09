32878b873542ac029a7f8c23b4923d5f6df35d2f
Mateus Vitor <mateusriograndense@gmail.com>
2021-08-08 21:32:59 -0300

    :recycle: Criando uma nova rota para atutenticação de usuário.

---
+++ redothis/resources/auth.py
@@ -0,0 +1,13 @@
+from flask import Blueprint
+from ..controllers.auth import auth_user
+
+
+def init(app):
+
+    bp = Blueprint('auth', __name__)
+
+    @bp.route("/oauth", methods=['POST'])
+    def auth():
+        return auth_user()
+
+    app.register_blueprint(bp, url_prefix="/api/v1")
